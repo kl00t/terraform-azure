39100ba6a301a149d1ace6f3d542276ba811abd8
Scott Vaughan <scott.vaughan@ensono.com>
2024-07-12 15:16:30 +0100

    Add template files

--- main.tf
+++ main.tf
@@ -102,3 +102,46 @@ resource "azurerm_network_interface" "mtc-nic" {
     environment = "dev"
   }
 }
+
+# Create a linux virtual machine
+resource "azurerm_linux_virtual_machine" "mtc-vm" {
+  name                  = "mtc-vm"
+  resource_group_name   = azurerm_resource_group.mtc-rg.name
+  location              = azurerm_resource_group.mtc-rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.mtc-nic.id]
+
+  # Template for installing Docker on the virtual machine
+  custom_data = filebase64("customdata.tpl")
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/mtcazurekey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("windows-ssh-script.tpl", {
+      hostname     = self.public_ip_address,
+      user         = "adminuser",
+      identityfile = "~/.ssh/mtcazurekey"
+    })
+    interpreter = ["Powershell", "-Command"]
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
